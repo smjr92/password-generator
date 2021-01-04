106785f4001dbaecb391eacb5f658d6a91b7f56b
Sergio Jr <45772955+smjr92@users.noreply.github.com>
2021-01-04 11:26:43 -0300

    Add files via upload
    A simple password generator with the following functions:  
    
    - Generate passwords with 8 characters, the first two with letters.  
    - First capital letter.  
    - Second lower case. 
    - Sequence of 4 random numbers.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Gerar_Senhas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnCopiar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGerar.Location = new System.Drawing.Point(48, 64);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(125, 28);
+            this.btnGerar.TabIndex = 0;
+            this.btnGerar.Text = "Gerar";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(48, 24);
+            this.txtSenha.MaxLength = 6;
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.ReadOnly = true;
+            this.txtSenha.Size = new System.Drawing.Size(125, 27);
+            this.txtSenha.TabIndex = 1;
+            this.txtSenha.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCopiar
+            // 
+            this.btnCopiar.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopiar.Location = new System.Drawing.Point(48, 98);
+            this.btnCopiar.Name = "btnCopiar";
+            this.btnCopiar.Size = new System.Drawing.Size(125, 27);
+            this.btnCopiar.TabIndex = 2;
+            this.btnCopiar.Text = "Copiar Senha";
+            this.btnCopiar.UseVisualStyleBackColor = true;
+            this.btnCopiar.Click += new System.EventHandler(this.btnCopiar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(48, 131);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(125, 27);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(217, 182);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnCopiar);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.btnGerar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gerador de Senhas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnCopiar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gerar_Senhas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+          
+            string chars = "0123456789";
+            string chars1 = "qwertyuiopasdfghjklzxcvbnm";
+            string chars2 = "QWERTYUIOPASDFGHJKLZXCVBNM";
+            string pass = "";
+            string pass1 = "";
+            string pass2 = "";
+            Random random = new Random();
+            for (int x = 0; x < 4; x++)
+            {
+                pass = pass + chars.Substring(random.Next(0, chars.Length - 1), 1);
+            }
+            for (int x = 0; x < 1; x++)
+            {
+                pass1 = pass1 + chars1.Substring(random.Next(0, chars1.Length - 1), 1);
+            }
+            for (int x = 0; x < 1; x++)
+            {
+                pass2 = pass2 + chars2.Substring(random.Next(0, chars2.Length - 1), 1);
+            }
+            txtSenha.Text = pass2 + pass1 + pass;
+        }
+
+        private void btnCopiar_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtSenha.Text);
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
